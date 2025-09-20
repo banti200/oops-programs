f1e042276001e0475db4375b54d4152dad72001f
NayanCoder1 <nayanborse957@gmail.com>
2025-09-20 14:50:52 +0530

    Create interface.java

---
+++ interface.java
@@ -0,0 +1,22 @@
+// Interface Example (Abstraction + Polymorphism)
+interface Animal {
+    void sound();
+}
+
+class Dog implements Animal {
+    public void sound() { System.out.println("Dog barks "); }
+}
+
+class Cat implements Animal {
+    public void sound() { System.out.println("Cat meows "); }
+}
+
+public class InterfaceDemo {
+    public static void main(String[] args) {
+        Animal a1 = new Dog();
+        Animal a2 = new Cat();
+
+        a1.sound();
+        a2.sound();
+    }
+}
